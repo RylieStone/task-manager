eb101844042945addd084ca338261d25641bb863
Rylie <rylie.stone2005@gmail.com>
2025-11-19 12:35:47 -0500

    added body code

---
+++ src/modules/body.js
@@ -0,0 +1,17 @@
+import "./body.css"
+
+function Body(props) {
+    const tasks = props.tasks
+  return (
+    <div className="body">
+
+      <div className='task-body'>
+
+        <h1>hi</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default Body;
