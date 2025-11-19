5b077ccf68df3f0fa2c174878bade70ec8d4323e
Rylie <rylie.stone2005@gmail.com>
2025-11-19 15:55:28 -0500

    added server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = require('./router/router')
+const server = express()
+server.use(express.json())
+server.use('/auth', router)
+
+const port = process.env.PORT || 9000
+server.listen(port, () => {
+    console.log('server is running on 9000')
+})
