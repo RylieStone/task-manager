7091d65c098a888faef46a1f1bbcce1ec4a005eb
Rylie <rylie.stone2005@gmail.com>
2025-11-19 15:18:56 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 let uptasks = [{id: 1, title: "homework", status: false, created: '11:30'}, {id: 2, title: "coding", status: false, created: '11:11'}, {id: 3, title: "dishes", status: true, created: '11:35'}]
 function App() {
   useEffect(() => {
+    // add axios
     setTasks(uptasks)
   }, [])
   const [tasks, setTasks] = useState([])
   const change = (id) => {
     let newTasks = tasks.map((task) => task.id === id ? {...task, status: !task.status} : task)
     setTasks(newTasks)
+    // add axios
   }
   const del = (id) => {
     let newTasks = tasks.filter((task) => task.id !== id)
@@ -23,7 +25,7 @@ function App() {
         <h2>Task Manager</h2>
       </header>
 
-      <Body tasks={tasks} change={change} del={del}/>
+      <Body tasks={tasks} change={change} del={del} setTasks={setTasks}/>
 
     </div>
   );
