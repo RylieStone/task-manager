6db9e1c49e821908f2f10f2848a9b2fc8f79420d
Rylie <rylie.stone2005@gmail.com>
2025-11-19 11:05:29 -0500

    first migration

---
+++ data/migrations/20251119154617_taskDB.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.createTable('tasks', table => {
+    table.increments('task_id')
+    table.string('title').notNullable().unique()
+    table.boolean('status').notNullable()
+    table.string('created-at').notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('tasks')
+};
