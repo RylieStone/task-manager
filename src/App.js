9eaf235ab6d94149d20846645011548d69ab9bec
Rylie <rylie.stone2005@gmail.com>
2025-11-19 11:32:39 -0500

    added temp task list and use effect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
-
+const uptasks = [{title: "homework", status: false, created: '11:30'}, {title: "coding", status: false, created: '11:11'}, {title: "dishes", status: true, created: '11:35'}]
 function App() {
   useEffect(() => {
-
+    setTasks(uptasks)
   }, [])
-  const {tasks, setTasks} = useState()
+  const {tasks, setTasks} = useState([])
   return (
     <div className="App">
       <header className="App-header">
