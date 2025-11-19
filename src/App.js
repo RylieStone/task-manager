bf0d54a7d88b080b050dc926e47899c36ef91aa0
Rylie <rylie.stone2005@gmail.com>
2025-11-19 12:35:39 -0500

    added code for props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import Body from "./modules/body"
 import axios from 'axios'
 import './App.css';
 const uptasks = [{title: "homework", status: false, created: '11:30'}, {title: "coding", status: false, created: '11:11'}, {title: "dishes", status: true, created: '11:35'}]
@@ -7,16 +8,14 @@ function App() {
   useEffect(() => {
     setTasks(uptasks)
   }, [])
-  const {tasks, setTasks} = useState([])
+  const [tasks, setTasks] = useState([])
   return (
     <div className="App">
       <header className="App-header">
         <h2>Task Manager</h2>
       </header>
 
-      <div className='body'>
-        <h1>hi</h1>
-      </div>
+      <Body tasks={tasks}/>
 
     </div>
   );
