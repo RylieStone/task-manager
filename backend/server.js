72cc0ad77ca90491df37dfa74b247e79d969be0c
Rylie <rylie.stone2005@gmail.com>
2025-11-19 19:05:12 -0500

    updated server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const router = require('./router/router')
 const server = express()
+server.use(cors({
+    origin: 'http://localhost:3000'
+}))
 server.use(express.json())
 server.use(router)
-
 const port = process.env.PORT || 9000
 server.listen(port, () => {
     console.log('server is running on 9000')
